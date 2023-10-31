6368664ef1e8f12335ae797361e749a8833d20f5
joao7victor <147437448+joao7victor@users.noreply.github.com>
2023-10-31 16:33:07 -0300

    Add files via upload

---
+++ joao22.py
@@ -0,0 +1,4 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+for numero range (1, 50)
+if numero % 2 == 0:
+    print (numero)
